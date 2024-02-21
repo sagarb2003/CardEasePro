9209b31fa9afd26abf0652ae525650998ed62193
sagarb2003 <sagarssb2003@gmail.com>
2024-02-21 22:47:46 +0530

    fix:added view my card button

--- client/src/Pages/DashBoard.js
+++ client/src/Pages/DashBoard.js
@@ -52,13 +52,33 @@ export default function DashBoard(props) {
             <Link to="/">
               <ul className="cursor-pointer">
                 <li className="px-2 mt-2 py-1">
-                  <h1 className="lg:text-3xl text-lg font-bold">
-                    Expense Tracker
-                  </h1>
+                  <h1 className="lg:text-3xl text-lg font-bold">CardEasePro</h1>
                 </li>
               </ul>
             </Link>
             <ul className="lg:flex lg:flex-col mt-10 hidden">
+              <Link to="/dashboard/card">
+                <li
+                  onClick={selectLink1}
+                  className={
+                    active === "1"
+                      ? "mb-4 flex px-5 py-3 cursor-pointer text-2xl hover:bg-jp-black hover:rounded-md bg-jp-black rounded-md"
+                      : "mb-4 flex px-5 py-3 cursor-pointer text-2xl hover:bg-jp-black hover:rounded-md transition delay-100"
+                  }
+                >
+                  <span className="mr-3">
+                    {/* <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-7 w-7"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+                    </svg> */}
+                  </span>
+                  View My Card
+                </li>
+              </Link>
               <Link to="/dashboard">
                 <li
                   onClick={selectLink1}
