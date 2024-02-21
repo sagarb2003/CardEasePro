62b15d7f1c6e5e06879b648e7a14d4b7adb5cef4
sagarb2003 <sagarssb2003@gmail.com>
2024-02-21 22:44:45 +0530

    fix:changes in LandingPage

--- client/src/Pages/LandingPage.js
+++ client/src/Pages/LandingPage.js
@@ -131,12 +131,13 @@ export default function LandingPage(props) {
       <div className="lg:grid lg:grid-cols-2 text-slate-300 lg:h-5/6 mt-32 lg:mt-0">
         <div className="my-auto">
           <div className="lg:text-6xl lg:py-3 text-2xl p-1">
-            The{" "}
-            <span className="text-mj-yellow underline">Expense Tracker</span>{" "}
-            that works for you
+            Empower Your Finances <br />
+            with <span className="text-mj-yellow underline">
+              CardEasePro
+            </span>{" "}
           </div>
           <div className="lg:text-2xl py-2 text-xl mt-4">
-            Track all your expenses here...
+            Design Your Card, Master Your Budget...
           </div>
           {props.isLoggedIn ? (
             <button
@@ -160,24 +161,44 @@ export default function LandingPage(props) {
               </svg>
             </button>
           ) : (
-            <button
-              onClick={props.openModalSignup}
-              className="mt-8 bg-mj-yellow text-mj-black px-4 py-2 rounded-md font-semibold flex items-center w-fit my-2"
-            >
-              Get Started
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="pl-1 w-8"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+            <>
+              <button
+                onClick={props.openModalSignup}
+                className="mt-8 bg-mj-yellow text-mj-black px-4 py-2 rounded-md font-semibold flex items-center w-fit my-2"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
+                Get Started
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="pl-1 w-8"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+              <button
+                onClick={props.openModalSignup}
+                className="mt-8 bg-mj-yellow text-mj-black px-4 py-2 rounded-md font-semibold flex items-center w-fit my-2"
+              >
+                Get Virtual Card
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="pl-1 w-8"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </>
           )}
         </div>
         <div className="my-auto mt-20 w-full">
